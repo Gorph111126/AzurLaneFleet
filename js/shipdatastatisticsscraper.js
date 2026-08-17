2c628755efae37696e8ffef2e0927b74df5d8e15
Gorph111126 <granville.4@wright.edu>
2026-08-17 19:18:30 -0400

    need to add images, meta, and pr/dr ships. currently cant change to any of the new ships

--- js/shipdatastatisticsscraper.js
+++ js/shipdatastatisticsscraper.js
@@ -2,7 +2,7 @@ const stats = require('F:\\Users\\Downloads\\ship_data_statistics.json');
 const template = require('F:\\Users\\Downloads\\ship_data_template.json');
 const strengthen = require('F:\\Users\\Downloads\\ship_data_strengthen.json');
 
-const newships = ["Z47","U-31","Z43","Strasbourg"];
+const newships = ["Z47","U-31","Z43","Strasbourg","John Rodgers","Bennington","Vicksburg","Harrison","Collett","Griffin","Surrey","Superb","L'Intrépide","Arromanches","Cherbourg","Unzen-chan","Z15","Z14","Prinz Moritz","U-2501","Götz von Berlichingen","Musashi-chan","Nayoro","Kizu","Unryuu","Bogatyr","Krasny Kavkaz","Ozornoy","Moskva","Hai Chou","Chang Wu","Fujinami","Ugolino Vivaldi","Alberto di Giussano","Francesco Caracciolo","Pasadena","Clarence K. Bronson","Cowpens","Lexington II","Adventure","Royal James","Pearl","Lyme","Queen Anne's Revenge","Minase","Asama","Oumi","I-404","Taekaze","Hakuhou","Duguay-Trouin","Le Hardi","Duquesne","Bois Belleau","Masséna","Little Ägir","Z13","U-552","Admiral Zenker","Little Anchorage","Trinidad","Gallant","Cleopatra","Trafalgar","Oleg","Evertsen","De Zeven Provinciën","Kazan","Miller","Santa Fe","Franklin","Bartolomeo Colleoni","Giosuè Carducci","Maggiore Baracca","Maggiore Baracca","Raffaello","Fu Po","Chang Feng","Hai Yung","Chien Wu","Z11","Z9","Duisburg","Z52","Fritz Rumey","Dolphin","Portsmouth Adventure","Amity","Fancy","Ganj-i-Sawai","Suzunami","Ayase","Watarase","Amagi(CV)","Bell","Herring","Fargo","Pittsburgh","Pittsburgh","L'Audacieux"];
 const highestships ={};
 
 for (const ship of Object.values(stats)) {
@@ -20,6 +20,7 @@ for (const ship of Object.values(stats)) {
 
 const output = {}; 
 for (const ship of Object.values(highestships)){
+    let retrofitbonus = 0;
     let strid = Math.floor(ship.id/10);
     if(ship.retro == null){
         ship.retro = 0;
@@ -44,6 +45,7 @@ for (const ship of Object.values(highestships)){
         //data statistics ends here, now use data template
 
         uni_id: ship.id,
+        painting: ship.id,
         jp_name: ship.jp_name,
         en_name: ship.english_name,
         cn_name: ship.cn_name,
