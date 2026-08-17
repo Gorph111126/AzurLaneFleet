a69abaf2de320988f47f0396be65d8867dafd1dd
Gorph111126 <granville.4@wright.edu>
2026-08-17 16:18:50 -0400

    starting an automation to get the info from the datamine dump

---
+++ js/shipdatastatisticsscraper.js
@@ -0,0 +1,8 @@
+const shipstats = require('F:\\Users\\Downloads\\ship_data_template.json'); 
+
+const newships = ["Z47","U-31","Z43","Strasbourg"]
+
+
+let stats = shipstats["404061"]
+strengthid = stats.strengthen_id;
+console.log(stats.strengthen_id)
